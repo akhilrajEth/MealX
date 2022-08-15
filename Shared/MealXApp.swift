03ae427de4d8ad26e57305cf1e37397aa2ecd0f1
Akhil Maheepathi <akhil@maheeps.net>
2022-08-15 16:28:42 -0400

    Initial Commit

---
+++ Shared/MealXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealXApp.swift
+//  Shared
+//
+//  Created by Akhil Maheepathi on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MealXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
